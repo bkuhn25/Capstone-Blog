47c698086dcca402d0f887023ff88fca9f2d270b
Benjamin <bn.kuhn1@gmail.com>
2021-05-24 16:39:55 -0500

    modify for use with Postgres

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def admin_only(f):
 
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
@@ -34,7 +35,7 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
